da66325b33b076f27b658d2c24ff67ceccd76cf4
Aditya <ak.adityakumar421@gmail.com>
2024-05-25 18:53:48 +0530

    add vercel.json

--- quiz/settings.py
+++ quiz/settings.py
@@ -86,8 +86,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=35),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=50),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
 }
 
 LANGUAGE_CODE = 'en-us'
